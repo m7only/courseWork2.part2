0c3154e71c26e96e9320ec5ba2af06a47ca9b0b8
Only <ongranly@ngs.ru>
2023-01-20 20:02:47 +0700

    Курсовая 2го курса. Функциональное программирование

---
+++ src/WordCount.java
@@ -0,0 +1,23 @@
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class WordCount {
+    private final Map<String, Integer> words = new LinkedHashMap<>();
+    public WordCount(String string) {
+        for (String word : string.split("\\s")) {
+            if (words.containsKey(word)) {
+                words.put(word, words.get(word) + 1);
+            } else {
+                words.put(word, 1);
+            }
+        }
+    }
+
+    public void printResult(){
+        System.out.printf("В тексте %d слов%n", words.size());
+        words.entrySet().stream()
+                .limit(10)
+                .sorted(Map.Entry.<String, Integer>comparingByValue().reversed())
+                .forEach(entry -> System.out.println(entry.getValue() + " - " + entry.getKey()));
+    }
+}
